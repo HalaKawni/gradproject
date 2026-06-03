825885fab0c8efb5553b88bf084c341fb4bbc004
Nasser <nasseraker4@gmail.com>
2026-06-03 21:38:21 +0300

    fixes

--- server/app.js
+++ server/app.js
@@ -16,7 +16,7 @@ app.use(express.json({ limit: '50mb' }));
 const cors = require('cors');
 app.use(cors());
 
-app.use('/', userRouter)d
+app.use('/', userRouter);
 app.use('/api/builder', builderRoutes);
 app.use('/api/admin', adminRouter)
 app.use('/api/courses', coursesRouter);
