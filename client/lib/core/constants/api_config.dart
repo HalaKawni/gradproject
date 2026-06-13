18a0f3feccf89c6adace5ad5f0873493eed54540
hala <s12112519@stu.najah.edu>
2026-06-13 03:26:54 +0300

    comit

--- client/lib/core/constants/api_config.dart
+++ client/lib/core/constants/api_config.dart
@@ -2,6 +2,10 @@ import 'package:flutter/foundation.dart';
 
 class ApiConfig {
   static const String _hostOverride = String.fromEnvironment('API_HOST');
+  static const String _mobileHost = String.fromEnvironment(
+    'API_MOBILE_HOST',
+    defaultValue: '192.168.1.13',
+  );
   static const String _port = '3000';
 
   static String get host {
@@ -14,8 +18,9 @@ class ApiConfig {
       return browserHost.isNotEmpty ? browserHost : 'localhost';
     }
 
-    return defaultTargetPlatform == TargetPlatform.android
-        ? '10.0.2.2'
+    return defaultTargetPlatform == TargetPlatform.android ||
+            defaultTargetPlatform == TargetPlatform.iOS
+        ? _mobileHost
         : 'localhost';
   }
 
